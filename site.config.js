4419a3a97c4ec48747d1979a8492273ffe0774f6
MinusTen <xkjing@outlook.com>
2025-01-09 20:09:22 +0800

    Comment

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "comment",
       "issue-term": "og:title",
       label: "💬评论",
     },
