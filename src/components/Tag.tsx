df6a5d236e895779c2674c79b71904b530d465a4
MinusTen <xkjing@outlook.com>
2025-01-09 20:01:28 +0800

    Tag Colors

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,37 +1,51 @@
+import { colors } from "../styles/colors"
+import styled from "@emotion/styled"
 import { useRouter } from "next/router"
 import React from "react"
 
+const colorArray = [
+  colors.light.red4,
+  colors.light.amber4,
+  colors.light.green4,
+  colors.light.blue4,
+  colors.light.indigo4,
+  colors.light.purple4,
+  colors.light.pink4,
+]
+
 type Props = {
   children: string
-  tag_id: number
 }
 
-const Tag: React.FC<Props> = ({ children, tag_id }) => {
+const hashStringToColor = (str: string, colorsArray: string[]) => {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  const index = Math.abs(hash) % colorsArray.length
+
+  return colorsArray[index]
+}
+
+const Tag: React.FC<Props> = ({ children }) => {
   const router = useRouter()
+
   const handleClick = (value: string) => {
     router.push(`/?tag=${value}`)
   }
 
-  let arrayOfColors = [
-    "bg-red-200",
-    "bg-yellow-200",
-    "bg-green-200",
-    "bg-blue-200",
-    "bg-indigo-200",
-    "bg-purple-200",
-    "bg-pink-200",
-  ]
+  const StyledTag = styled.div`
+    background-color: ${hashStringToColor(children, colorArray)};
+    color: ${colors.light.gray10};
+    padding: 0.25rem 0.5rem;
+    border-radius: 50px;
+    font-size: 0.75rem;
+    line-height: 1rem;
+    font-weight: 400;
+    cursor: pointer;
+  `
 
-  return (
-    <div
-      onClick={() => handleClick(children)}
-      className={`text-xs text-gray-800 font-normal rounded-full ${
-        arrayOfColors[tag_id % arrayOfColors.length]
-      } px-2 py-1 cursor-pointer`}
-    >
-      {children}
-    </div>
-  )
+  return <StyledTag onClick={() => handleClick(children)}>{children}</StyledTag>
 }
 
 export default Tag
