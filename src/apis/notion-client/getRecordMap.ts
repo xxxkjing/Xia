7840dfabe18b7222e46abe38fcee457f5ec2f033
MetaIllusion <xkjing@outlook.com>
2025-10-30 18:30:24 +0800

    Update getRecordMap.ts

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -2,6 +2,7 @@ import { NotionAPI } from "notion-client"
 
 export const getRecordMap = async (pageId: string) => {
   const api = new NotionAPI({
+    apiBaseUrl: "https://physicalmouse.notion.site/api/v3",
     authToken: process.env.NOTION_TOKEN_V2,
   })
   const recordMap = await api.getPage(pageId)
